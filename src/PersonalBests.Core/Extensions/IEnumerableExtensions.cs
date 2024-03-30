06542c0020bfc02f492719ca043af2c8183493b1
Adam Hill <adam.hill@outlook.com>
2024-03-30 13:17:04 +0000

    Added CSV writing.

--- src/PersonalBests.Core/Extensions/IEnumerableExtensions.cs
+++ src/PersonalBests.Core/Extensions/IEnumerableExtensions.cs
@@ -1,7 +1,35 @@
-﻿namespace PersonalBests.Extensions;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+using System.Text;
+
+namespace PersonalBests.Extensions;
 
 public static class IEnumerableExtensions
 {
     public static List<TResult> Aggregate<TSource, TResult>(this IEnumerable<TSource> source, Func<List<TResult>, TSource, List<TResult>> func)
         => source.Aggregate(new List<TResult>(), (acc, current) => func(acc, current));
+
+    public static async Task ToCsvFile<T>(this IEnumerable<T> source, string filePath)
+    {
+        using var writer = new StreamWriter(filePath, append: false, Encoding.UTF8);
+        using var csv = new CsvWriter(writer, CultureInfo.CurrentCulture);
+        await csv.WriteRecordsAsync(source);
+    }
+
+    public static async Task<byte[]> ToCsvBytes<T>(this IEnumerable<T> source)
+        => Encoding.UTF8.GetBytes(await source.ToCsvString<T>());
+
+    public static async Task<string> ToCsvString<T>(this IEnumerable<T> source)
+    {
+        using var stream = new MemoryStream();
+        using var reader = new StreamReader(stream, Encoding.UTF8);
+        using var writer = new StreamWriter(stream, Encoding.UTF8);
+        using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.CurrentCulture));
+        await csv.WriteRecordsAsync(source);
+        writer.Flush();
+        stream.Position = 0;
+
+        return await reader.ReadToEndAsync();
+    }
 }
--- src/PersonalBests.Tests/ReportGeneratorTests.cs
+++ src/PersonalBests.Tests/ReportGeneratorTests.cs
@@ -3,6 +3,7 @@ using PersonalBests.Models;
 using PersonalBests.Services;
 using PersonalBests.Tests.Mocks;
 using PersonalBests.Tests.TestData;
+using PersonalBests.Extensions;
 
 namespace PersonalBests.Tests;
 
@@ -11,10 +12,26 @@ public class PersonalBestTests
     private ReportGenerator CreateReportGenerator(List<ScoreRecord> dataToProvide) => new ReportGenerator(new MockScoreProvider(dataToProvide));
 
     [Fact]
-    public void TotalCount_AsExpected()
+    public void TotalCountAsExpected()
         => CreateReportGenerator(Data.AllScores)
             .Generate()
             .Count()
             .Should()
             .Be(16);
+
+    [Fact]
+    public async Task CsvFileProduced()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "test.csv");
+        
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+
+        var data = CreateReportGenerator(Data.AllScores).Generate();
+        await data.ToCsvFile(path);
+        File.Exists(path).Should().BeTrue();
+        File.Delete(path);
+    }
 }
